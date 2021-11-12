0eddaeda50e2b62d93abeb34474059136a5dc7af
Mariah A. Knowles <snotskie@gmail.com>
2021-11-12 19:13:04 +0000

    Backup on: Fri Nov 12 19:13:04 UTC 2021

---
+++ save.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+git config --global user.email "snotskie@gmail.com"
+git config --global user.name "Mariah A. Knowles"
+git add --all
+git commit -m "Backup on: `date`"
+git push
