af4a80399ff4eb3d5d65178805d99318e0b8ab2b
snotskie <snotskie@gmail.com>
2021-11-23 11:53:17 -0600

    Update save.sh

--- save.sh
+++ save.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env sh
 
-git pull
-git config --global user.email "snotskie@gmail.com"
 git config --global user.name "Mariah A. Knowles"
-git add --all
-git commit -m "Backup on: `date`"
-git push
+git config --global user.email "snotskie@gmail.com"
